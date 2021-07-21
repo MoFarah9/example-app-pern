a0df4b2c0a43624fb96d7100b43ae413a7b7295c
Mohammed Farah <m.farah009@gmail.com>
2021-07-21 08:52:07 +0200

    added react router and jodit-react and thier boilerplate

--- client/src/App.js
+++ client/src/App.js
@@ -1,4 +1,6 @@
-import Article from './components/Article'
+import { Route, Switch } from 'react-router-dom'
+import NewArticle from './pages/NewArticle'
+import Home from './pages/Home'
 
 export default function App() {
   return (
@@ -6,9 +8,19 @@ export default function App() {
       <header>
         <div>Example Website</div>
       </header>
-      <main>
-        <Article id="1" />
-      </main>
+      <Switch>
+        <Route exact path="/">
+          <Home />
+        </Route>
+        <Route path="/new-article">
+          <NewArticle />
+        </Route>
+        <Route>
+          <main>
+            <h3>Page not found</h3>
+          </main>
+        </Route>
+      </Switch>
     </div>
   )
 }
---
+++ client/src/pages/Home.js
@@ -0,0 +1,10 @@
+import React from 'react'
+import Article from '../components/Article'
+
+export default function Home() {
+  return (
+    <main>
+      <Article id="1" />
+    </main>
+  )
+}
