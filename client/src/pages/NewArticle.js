3d929ec5d2a370db5f673af4b26bc2a6911329bc
Mohammed Farah <m.farah009@gmail.com>
2021-07-22 02:45:46 +0200

    removed jodit react

--- client/src/pages/NewArticle.js
+++ client/src/pages/NewArticle.js
@@ -1,36 +1,15 @@
-import React, { useState, useRef } from 'react'
-import JoditEditor from 'jodit-react'
+import React from 'react'
 
 export default function NewArticle() {
-  const editor = useRef(null)
-  const [content, setContent] = useState('')
-
-  const config = {
-    readonly: false, // all options from https://xdsoft.net/jodit/doc/
-    minHeight: 300,
-  }
-
-  function handleSubmit(event) {
-    event.preventDefault()
-    console.log(content)
-  }
-
   return (
     <main>
       <h2>Write new article</h2>
-      <form onSubmit={handleSubmit}>
+      {/* <form onSubmit={handleSubmit}>
         <div>
-          <JoditEditor
-            ref={editor}
-            value={content}
-            config={config}
-            tabIndex={1} // tabIndex of textarea
-            onBlur={(newContent) => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
-            onChange={(newContent) => {}}
-          />
+
           <button type="submit">Submit</button>
         </div>
-      </form>
+      </form> */}
     </main>
   )
 }
