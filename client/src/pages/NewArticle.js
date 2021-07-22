18f571c82f2fd55a7fed1c2882e42b689f313854
Mohammed Farah <m.farah009@gmail.com>
2021-07-22 02:42:49 +0200

    added basic jodit editor setup in NewArticle

--- client/src/pages/NewArticle.js
+++ client/src/pages/NewArticle.js
@@ -7,22 +7,30 @@ export default function NewArticle() {
 
   const config = {
     readonly: false, // all options from https://xdsoft.net/jodit/doc/
+    minHeight: 300,
+  }
+
+  function handleSubmit(event) {
+    event.preventDefault()
+    console.log(content)
   }
 
   return (
     <main>
       <h2>Write new article</h2>
-      <div>
-        <JoditEditor
-          ref={editor}
-          value={content}
-          config={config}
-          tabIndex={1} // tabIndex of textarea
-          onBlur={(newContent) => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
-          onChange={(newContent) => {}}
-        />
-        <button>Submit</button>
-      </div>
+      <form onSubmit={handleSubmit}>
+        <div>
+          <JoditEditor
+            ref={editor}
+            value={content}
+            config={config}
+            tabIndex={1} // tabIndex of textarea
+            onBlur={(newContent) => setContent(newContent)} // preferred to use only this option to update the content for performance reasons
+            onChange={(newContent) => {}}
+          />
+          <button type="submit">Submit</button>
+        </div>
+      </form>
     </main>
   )
 }
